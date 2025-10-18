defc7ae4d93113204f559645c1835b3d6217bb48
EminemBack <mohamedleminebelly@gmail.com>
2025-10-18 23:31:08 +0000

    fix: sql fields

--- database/init/03-add-phone-number.sql
+++ database/init/03-add-phone-number.sql
@@ -1,5 +1,5 @@
 -- Add phone_number column to containers table
 ALTER TABLE containers ADD COLUMN IF NOT EXISTS phone_number VARCHAR(50);
 
--- Add comment
+-- Add comment this comment to phone_number column
 COMMENT ON COLUMN containers.phone_number IS 'Contact phone number of the responsible person';
